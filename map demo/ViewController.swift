71c11fd51f66f903cbc9cd353a87d2847f4a1a3a
Animesh Manglik <amanglik@whalerockindustries.com>
2016-12-13 12:43:06 -0800

    Initial Commit

---
+++ map demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  map demo
+//
+//  Created by Animesh Manglik on 12/13/16.
+//  Copyright © 2016 Animesh Manglik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
